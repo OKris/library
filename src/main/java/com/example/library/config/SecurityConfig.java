a4208b5c99f6b0abbb1a0cadf0749379424f052a
OKris <oim.kristina@gmail.com>
2026-03-16 12:20:05 +0200

    right url

--- src/main/java/com/example/library/config/SecurityConfig.java
+++ src/main/java/com/example/library/config/SecurityConfig.java
@@ -57,6 +57,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
+        //config.setAllowedOrigins(List.of("http://localhost:5173"));
         config.setAllowedOrigins(List.of(frontendUrl));
         //config.setAllowedOrigins(List.of("*")); // RestControllerist võin ära võtta @CrossOrigin
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "PATCH"));
